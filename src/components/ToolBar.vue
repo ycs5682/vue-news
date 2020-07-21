328ee12068cb0d88e1a024b95abbf362c7eed04a
유창선 <ycs5682@gmail.com>
2020-07-21 17:39:35 +0900

    ToolBarのリングスタイリング

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    <router-link to="/news">News</router-link>
-    <router-link to="/ask">Ask</router-link>
-    <router-link to="/jobs">Jobs</router-link>
+  <div class="header">
+    <router-link to="/news">News</router-link> | <router-link to="/ask">Ask</router-link> |
+    <router-link to="/jobs">Jobs</router-link> |
   </div>
 </template>
 
-<script>
-export default {};
-</script>
-
-<style></style>
+<style scoped>
+.header {
+  color: white;
+  background-color: #42b883;
+  display: flex;
+  padding: 8px;
+}
+.header .router-link-exact-active {
+  color: #35495e;
+}
+.header a {
+  color: white;
+}
+</style>
