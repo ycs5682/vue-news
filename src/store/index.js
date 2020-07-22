5e22f25f977cd260d3c97c36e34f80bc9702729a
유창선 <ycs5682@gmail.com>
2020-07-22 12:00:08 +0900

    JobsViewにstoreの適用

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,36 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList } from '../api/index.js';
+import { fetchNewsList, fetchJobsList } from '../api/index.js';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    news: []
+    news: [],
+    jobs: []
   },
   mutations: {
     SET_NEWS(state, news) {
       state.news = news;
+    },
+    SET_JOBS(state, jobs) {
+      state.jobs = jobs;
     }
   },
   actions: {
     FETCH_NEWS(context) {
       fetchNewsList()
-        .then((response) => {
-          context.commit('SET_NEWS', response.data);
+        .then(({ data }) => {
+          context.commit('SET_NEWS', data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    FETCH_JOBS({ commit }) {
+      fetchJobsList()
+        .then(({ data }) => {
+          commit('SET_JOBS', data);
         })
         .catch((error) => {
           console.log(error);
