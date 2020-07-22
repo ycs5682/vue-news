1a907fcc5650ccf89fa49d29814ea28fecfadf24
유창선 <ycs5682@gmail.com>
2020-07-22 11:27:18 +0900

    NewsViewにactionsとmutations適用

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,21 +1,13 @@
 <template>
   <div>
-    <div v-for="user in users" v-bind:key="user.title">{{ user.title }}</div>
+    <div v-for="user in this.$store.state.news" v-bind:key="user.title">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index.js';
 export default {
-  data() {
-    return {
-      users: []
-    };
-  },
   created() {
-    fetchNewsList()
-      .then((response) => (this.users = response.data))
-      .catch((error) => console.log(error));
+    this.$store.dispatch('FETCH_NEWS');
   }
 };
 </script>
