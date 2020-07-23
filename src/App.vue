f07c3256fb4c6f969d87088565fdaf58831c8a4e
유창선 <ycs5682@gmail.com>
2020-07-23 23:08:15 +0900

    Newsリストのスタイリング

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,17 @@ body {
   padding: 0;
   margin: 0;
 }
+a {
+  text-decoration: none;
+  color: #34495e;
+}
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+a.router-link-exact-active {
+  text-decoration: underline;
+}
 .page-enter-active,
 .page-leave-active {
   transition: opacity 0.5s;
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    <p v-for="item in fetchedNews" :key="item.title">
-      <a :href="item.url">
-        {{ item.title }}
-      </a>
-      <small
-        >{{ item.time_ago }} by
-        <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
-      </small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in fetchedNews" :key="item.title" class="post">
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="new-title">
+            <a :href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link :to="`/user/${item.user}`" class="link-text">{{ item.user }} </router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -25,4 +34,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.news-list {
+  padding: 0;
+  margin: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
