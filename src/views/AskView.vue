8605f15beb80840d48f042fcd629ec8390797565
유창선 <ycs5682@gmail.com>
2020-07-24 10:09:59 +0900

    Ask,Jobsリストのスタイリング

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <p v-for="item in fetchedAsk" :key="item.title">
-      <router-link :to="`item/${item.id}`">
-        {{ item.title }}
-      </router-link>
-      <small>{{ item.time_ago }} by {{ item.user }}</small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in fetchedAsk" :key="item.title" class="post">
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="new-title">
+            <router-link :to="`item/${item.id}`">
+              {{ item.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <router-link :to="`/user/${item.user}`" class="link-text">{{ item.user }} </router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -21,4 +33,29 @@ export default {
   }
 };
 </script>
-<style></style>
+<style scoped>
+.news-list {
+  padding: 0;
+  margin: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
+    <ul class="news-list">
+      <li v-for="job in fetchedJobs" :key="job.title" class="post">
+        <div class="points">
+          {{ job.points || 0 }}
+        </div>
+        <div>
+          <p class="new-title">
+            <a :href="job.url">
+              {{ job.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            {{ job.time_ago }} by
+            <a :href="job.url">{{ job.domain }} </a>
+          </small>
+        </div>
+      </li>
+    </ul>
+
     <p v-for="job in fetchedJobs" :key="job.title">
       <a :href="job.url">
         {{ job.title }}
@@ -22,4 +41,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.news-list {
+  padding: 0;
+  margin: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
